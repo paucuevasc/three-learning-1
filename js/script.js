f33600a24fdb1d8d9a75ea03488718a91d807ebc
Pau Cuevas <pau.cuevas.c@hotmail.com>
2018-08-28 12:10:08 +0200

    probe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var scene = new THREE.Scene();
+var scene1 = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
