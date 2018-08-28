36b85d56f079a8b3152010d59f52728a19d8eef6
Pau Cuevas <pau.cuevas.c@hotmail.com>
2018-08-28 12:52:24 +0200

    cube

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-var scene1 = new THREE.Scene();
+var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var update = function(){
+//Create the shape
+var geometry=new THREE.BoxGeometry(1,1,1);
 
+//Create a material, colour or image texture
+var material = new THREE.MeshBasicMaterial({color:0xFFFFFF,wireframe:false});
+var cube=new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z=3;
+
+var update = function(){
+cube.rotation.x +=0.01;
+cube.rotation.y +=0.01;
 };
 
 var render = function(){
 renderer.render(scene,camera);
 };
+
 
 var GameLoop=function(){
 requestAnimationFrame(GameLoop);
